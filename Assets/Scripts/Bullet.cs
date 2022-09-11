e79cfc935fa397a1b67b0f50de7e554e7e1bace7
Daeun Jeong <jdoeun2@gmail.com>
2022-09-11 16:39:41 -0700

    Completed player movement and attack.
    Added basic prefabs too.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float timer;
+    [SerializeField] private float lifeCycle;
+
+    private void Start()
+    {
+        timer = 0.0f;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= lifeCycle)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyController>().healthPoint -= 1;
+        }
+
+        if (collision.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+    private Rigidbody2D rigidBody;
+    private Vector2 playerDirection;
+
+    private float timer;
+    [SerializeField] private float coolDown;
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private List<Transform> firePointOffset;
+    [SerializeField] private float fireForce;
+    public int bulletNumber;
+
+    void Start()
+    {
+        timer = 0.0f;
+        //bulletNumber = 1;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Fire()
+    {
+        for (int i = 0; i < bulletNumber; ++i)
+        {
+            Vector3 updatedPosition = gameObject.transform.localPosition + firePointOffset[i].localPosition;
+            GameObject bullet = Instantiate(bulletPrefab, updatedPosition, firePointOffset[i].localRotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.right * fireForce, ForceMode2D.Force);
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if(timer > coolDown)
+        {
+            Fire();
+            timer = 0.0f;
+        }
+
+        playerDirection = new Vector2(x, y).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + playerDirection * playerSpeed * Time.fixedDeltaTime);
+    }
+}
