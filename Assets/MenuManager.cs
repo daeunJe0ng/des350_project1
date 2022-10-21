810fca28eb912031c3f6bcef16c17c4eda9de899
Daeun Jeong <jdoeun2@gmail.com>
2022-10-21 02:09:23 -0700

    Implemented level BGM and menu button SFXs.

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -7,26 +7,33 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject titleScreenPanel;
     public GameObject howToPlayPanel;
+    public AudioSource startButtonSFX;
+    public AudioSource howToPlayButtonSFX;
+    public AudioSource BackButtonSFX;
 
     public void GameStart()
     {
+        startButtonSFX.Play();
         SceneManager.LoadScene("Level");
     }
 
     public void ShowHowToPlay()
     {
+        howToPlayButtonSFX.Play();
         titleScreenPanel.SetActive(false);
         howToPlayPanel.SetActive(true);
     }
 
     public void HideHowToPlay()
     {
+        BackButtonSFX.Play();
         titleScreenPanel.SetActive(true);
         howToPlayPanel.SetActive(false);
     }
 
     public void Quit()
     {
+        howToPlayButtonSFX.Play();
         Application.Quit();
     }
 }
